7f01322f64b7847ac6a8d39fbc60c12d90254543
Irene Goff <ireneegoff@gmail.com>
2024-05-31 11:40:31 -0400

    Implement draw method for Ball class.

--- cc6.js
+++ cc6.js
@@ -10,4 +10,12 @@ class Ball {
         this.color = color;
     }
 
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+
     
